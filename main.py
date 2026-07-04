64a3d80bae280691ac9cf3fc65a2ffed2cd558cd
Tracck <85881232+Tracck@users.noreply.github.com>
2026-07-04 11:14:56 +0700

    i give up

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from Services import UIService
-from Services.FileLoader import FileLoader
 
 from Classes.StorageManager import StorageManager
 from Classes.ProductEnum import ProductEnum
 
 manager = StorageManager()
 prodEnum = ProductEnum()
-
-DEFAULT_SAVE_FILE = "Data/Data.txt"
 
 UIService.Init(manager, prodEnum)
